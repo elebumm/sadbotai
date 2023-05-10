3e98f43bfcbf4c73d0db7c7e8d99a68a2688b696
Lewis Menelaws <lewis@tmrrwinc.ca>
2023-05-10 15:10:03 -0400

    Test

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ client = boto3.client(
     "comprehend",
     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_region_name="us-east-1",
+    region_name="us-east-1",
 )
 
 
